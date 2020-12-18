1f5e0ddb14d3e06a62dcaa2fdcf317016fd79853
sashulkin <sashula_94@mail.ru>
2020-12-18 20:05:02 +0900

    добавили новые файлы

--- web_python/app.py
+++ web_python/app.py
@@ -11,6 +11,20 @@ def index_page():
     """
     return render_template('index.html')
 
+@app.route('/product')
+def product_page():
+    """
+    Функция логики страницы 'product page'
+    """
+    return render_template('product.html')
+
+@app.route('/contact')
+def contact_page():
+    """
+    Функция логики страницы 'contact page'
+    """
+    return render_template('contact.html')
+
 # конструкция запуска 
 if __name__ == "__main__":
     app.run(debug=True)
